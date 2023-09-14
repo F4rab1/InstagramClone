2f34d053f652f03333a48efadfe8083a21b256c7
F4rab1 <issafarabi2022@gmail.com>
2023-09-14 21:09:33 +0600

    Initial Commit

---
+++ InstagramFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramFirebase
+//
+//  Created by Фараби Иса on 14.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
