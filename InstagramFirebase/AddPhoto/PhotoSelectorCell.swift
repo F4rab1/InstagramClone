ecad7c1918e59542757e50ffd7390a0811af22a7
F4rab1 <issafarabi2022@gmail.com>
2023-12-06 23:25:42 +0600

    Fetching User Photos

---
+++ InstagramFirebase/AddPhoto/PhotoSelectorCell.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoSelectorCell.swift
+//  InstagramFirebase
+//
+//  Created by Фараби Иса on 06.12.2023.
+//
+
+import UIKit
+
+class PhotoSelectorCell: UICollectionViewCell {
+    
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
